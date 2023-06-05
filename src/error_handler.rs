7c0bde2886275bdf0814cb04fd7ac9035a67611e
antonovmike <antonovma@outlook.com>
2023-06-05 13:37:45 +0500

    fmt error_handler

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -1,26 +1,26 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("failed to open spreadsheet file: {0}")]
-    TableErr(#[from] calamine::Error),
+    Table(#[from] calamine::Error),
 
     #[error("carapax ExecuteError: {0}")]
-    CarapaxErr(#[from] carapax::ExecuteError),
+    Carapax(#[from] carapax::ExecuteError),
 
     #[error("TryFromIntError: {0}")]
-    VectorErr(#[from] std::num::TryFromIntError),
+    Vector(#[from] std::num::TryFromIntError),
 
     #[error("InputFileErr: {0}")]
-    InputFileErr(#[from] std::io::Error),
+    InputFile(#[from] std::io::Error),
 
     #[error("sql error: {0}")]
-    SqlError(#[from] sqlite::Error),
+    Sqlite(#[from] sqlite::Error),
 
     #[error("CARAPAX_TOKEN is not set: {0}")]
-    EnvError(#[from] std::env::VarError),
+    Env(#[from] std::env::VarError),
 
     #[error("failled to create API: {0}")]
-    ApiError(#[from] carapax::ApiError),
+    Api(#[from] carapax::ApiError),
 
     #[error("failled to make caption bold: {0}")]
-    CaptionError(#[from] carapax::types::TextEntityError),
+    Caption(#[from] carapax::types::TextEntityError),
 }
